27c84c22ff571dc71b6bd42017a85153586dca42
Kevin Jung <kev.jung.ii@gmail.com>
2021-06-05 02:17:39 -0400

    adds bcrypt and user model

--- messaging-app-backend/app/models/user.rb
+++ messaging-app-backend/app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+  has_secure_password
+  has_many :messages
+  has_many :user_conversations
+  has_many :conversations, through: :user_conversations
+  has_many :followers, foreign_key: :follower_id , class_name: "Friendship"
+  has_many :followed, through: :followers
+  has_many :followed, foreign_key: :followed_id, class_name: "Friendship"
+  has_many :followers, through: :followed
+  validates_presence_of :username, :password
+  validates :password, uniqueness: true
 end
