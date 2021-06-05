c425d365870b5e6dbb557c3045a50e976659a350
Kevin Jung <kev.jung.ii@gmail.com>
2021-06-05 13:25:17 -0400

    adds notes too user

--- messaging-app-backend/app/models/user.rb
+++ messaging-app-backend/app/models/user.rb
@@ -1,12 +1,17 @@
 class User < ApplicationRecord
   has_secure_password
+
   has_many :messages
   has_many :user_conversations
   has_many :conversations, through: :user_conversations
+
+  #sets up self reference through Friendship model
   has_many :followers, foreign_key: :follower_id , class_name: "Friendship"
   has_many :followed, through: :followers
   has_many :followed, foreign_key: :followed_id, class_name: "Friendship"
   has_many :followers, through: :followed
+
+  #validations
   validates_presence_of :username, :password
   validates :password, uniqueness: true
 end
