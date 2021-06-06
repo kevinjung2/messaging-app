87622f857d227aabe7d510599a62344145685a8d
Kevin Jung <kev.jung.ii@gmail.com>
2021-06-06 01:28:02 -0400

    adds container componenets to app

--- messaging-app-frontend/src/App.js
+++ messaging-app-frontend/src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import React from 'react'
+import ConversationContainer from './containers/ConversationContainer'
+import Conversations from './containers/Conversations'
+import FriendsContainer from './containers/FriendsContainer'
 
 function App() {
   return (
     <div className="App">
-
+      <ConversationContainer />
+      <Conversations />
+      <FriendsContainer />
     </div>
   );
 }
