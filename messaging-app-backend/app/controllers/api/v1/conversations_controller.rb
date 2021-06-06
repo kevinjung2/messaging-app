a4e5ff1cddee18036c5a165decf1f7f95c073c07
Kevin Jung <kev.jung.ii@gmail.com>
2021-06-05 23:55:42 -0400

    adds basic conversation controller

--- messaging-app-backend/app/controllers/api/v1/conversations_controller.rb
+++ messaging-app-backend/app/controllers/api/v1/conversations_controller.rb
@@ -1,13 +1,19 @@
 class Api::V1::ConversationsController < ApplicationController
   def index
-
+    conversations = current_user.conversations.map { |conversation| ConversationSerializer.new(conversation) }
+    render json: { conversations: conversations }
   end
 
   def show
-
+    conversation = Conversation.find_by(id: params[:conversation_id])
+    render json: { conversation: ConversationSerializer.new(conversation) }
   end
 
   def create
-
+    new_convo = Conversation.create(name: params[:conversation][:name])
+    users = params[:conversation][:user_ids].map { |user_id| User.find_by(id: user_id)}
+    users.each do | user |
+      Message.create(content: "INIT_MESSAGE", user: user, conversation: new_convo)
+    end
   end
 end
