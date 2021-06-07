4011d8668d01efeb4233e4e87e401a45cc13be6a
Kevin Jung <kev.jung.ii@gmail.com>
2021-06-07 03:19:51 -0400

    adds feature to create an account

---
+++ messaging-app-frontend/src/Signup.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+
+class Signup extends Component {
+  state = {
+    username: "",
+    password: "",
+    redirect: null,
+    error: null
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    fetch('http://localhost:3000/api/v1/users', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({user: this.state})
+    }).then(resp => resp.json())
+    .then(token => this.handleSignup(token))
+    this.setState({
+      username: "",
+      password: "",
+    })
+  }
+
+  handleSignup = (token) => {
+    console.log(token);
+    if (token.jwt) {
+      this.props.login(token.jwt)
+      this.setState({
+        redirect: '/messenger'
+      }) else {
+        this.setState({
+          error: "Invalid Username or Password"
+        })
+      }
+    }
+  }
+
+  render(){
+    return(
+      <div className="signup">
+        {this.state.redirect ? <Redirect to={this.state.redirect} />}
+        {this.state.error ? <p>{this.state.error}</p>}
+        <form onSubmit={this.handleSubmit}>
+          <label>Username: </label>
+          <input type="text" name="username" onChange={this.handleChange} value={this.state.username} />
+          <label>Password: </label>
+          <input type="password" name="password" onChange={this.handleChange} value={this.state.password} />
+          <input type="submit" value="Sign Up" />
+        </form>
+      </div>
+    )
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    login: token => dispatch({type: "AUTHORIZE", token: token })
+  }
+}
+
+export default connect(null, mapDispatchToProps)(Signup)
