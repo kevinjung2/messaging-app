bedc0feff9cafeefe3e053d39a206558528387bc
Kevin Jung <kev.jung.ii@gmail.com>
2021-06-17 03:44:12 -0400

    makes it so init messages are not sent with conversation info

--- messaging-app-backend/app/models/conversation.rb
+++ messaging-app-backend/app/models/conversation.rb
@@ -3,4 +3,8 @@ class Conversation < ApplicationRecord
   has_many :users, through: :messages
 
   validates_presence_of :name
+
+  def messages
+    Message.where("conversation_id = ? and content != ?", "#{self.id}", "INIT_MESSAGE")
+  end
 end
