21af068821d779e589e2fe8e5c5cb68cc039a323
Kevin Jung <kev.jung.ii@gmail.com>
2021-06-16 23:51:53 -0400

    changes the way the date is formatted when sent throught the api

--- messaging-app-backend/app/models/message.rb
+++ messaging-app-backend/app/models/message.rb
@@ -3,4 +3,8 @@ class Message < ApplicationRecord
   belongs_to :user
 
   validates :content, presence: true
+
+  def created_at_formatted
+    self.created_at.strftime(" %b %d, %y - %l:%M %P ")
+  end
 end
--- messaging-app-backend/app/serializers/message_serializer.rb
+++ messaging-app-backend/app/serializers/message_serializer.rb
@@ -1,5 +1,5 @@
 class MessageSerializer < ActiveModel::Serializer
-  attributes :content, :id, :created_at, :user
+  attributes :content, :id, :created_at_formatted, :user
   belongs_to :conversation
   belongs_to :user
 end
