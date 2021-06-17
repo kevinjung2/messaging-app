162148ccc759c408107f928daedd66082fe7f9f6
Kevin Jung <kev.jung.ii@gmail.com>
2021-06-17 01:51:11 -0400

    handles imports for thunk and composes store enhancers together

--- messaging-app-frontend/src/index.js
+++ messaging-app-frontend/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MessagingApp from './MessagingApp';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers/index';
+import thunk from 'redux-thunk';
 
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const composedEnhancer = compose(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(thunk))
+const store = createStore(rootReducer, undefined, composedEnhancer)
 
 
 ReactDOM.render(
